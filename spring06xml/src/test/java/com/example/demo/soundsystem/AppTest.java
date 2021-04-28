f12e7a1ebd904b969f0b9a9bf0357f2892166aab
Xingchen Xiao <xiaoxing@msu.edu>
2021-04-28 12:02:45 +0800

    update

---
+++ spring06xml/src/test/java/com/example/demo/soundsystem/AppTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.soundsystem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-property.xml")
+public class AppTest {
+
+    @Test
+    public void test(){
+
+    }
+
+
+}
